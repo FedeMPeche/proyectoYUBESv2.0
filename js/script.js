e77b12240813ff9acf7b66c60c092a0a0c5ab5f3
lucasnrodriguez <lucasrodriguezlks@gmail.com>
2024-05-31 17:15:05 -0300

    barra de navegación

--- js/script.js
+++ js/script.js
@@ -148,7 +148,7 @@ document.addEventListener('DOMContentLoaded', function () {
   function changeActiveLink() {
     let index = sections.length;
 
-    while (--index && window.scrollY + navbarHeight + 1 < sections[index].offsetTop) {}
+    while (--index && window.scrollY + navbarHeight + 1 < sections[index].offsetTop) { }
 
     navLinks.forEach((link) => link.classList.remove('active'));
 
@@ -193,36 +193,36 @@ document.addEventListener('DOMContentLoaded', () => {
   const tabs = document.querySelectorAll('.seccionesCarrusel li');
   const carruselContainers = document.querySelectorAll('.carrusel-container');
 
-// Función para ocultar todas las galerías y mostrar solo la activa
+  // Función para ocultar todas las galerías y mostrar solo la activa
   function showActiveGallery(activeTab) {
-      carruselContainers.forEach(container => {
-          container.classList.remove('active');
-      });
+    carruselContainers.forEach(container => {
+      container.classList.remove('active');
+    });
 
-      if (activeTab.id === 'pestañaShows') {
-          document.querySelector('.carrusel-container.shows').classList.add('active');
-      } else if (activeTab.id === 'pestañaProduccion') {
-          document.querySelector('.carrusel-container.produccion').classList.add('active');
-      } else if (activeTab.id === 'pestañaIntimidad') {
-          document.querySelector('.carrusel-container.intimidad').classList.add('active');
-      } else if (activeTab.id === 'pestañaMas') {
-          document.querySelector('.carrusel-container.mas').classList.add('active');
-      }
+    if (activeTab.id === 'pestañaShows') {
+      document.querySelector('.carrusel-container.shows').classList.add('active');
+    } else if (activeTab.id === 'pestañaProduccion') {
+      document.querySelector('.carrusel-container.produccion').classList.add('active');
+    } else if (activeTab.id === 'pestañaIntimidad') {
+      document.querySelector('.carrusel-container.intimidad').classList.add('active');
+    } else if (activeTab.id === 'pestañaMas') {
+      document.querySelector('.carrusel-container.mas').classList.add('active');
+    }
   }
 
   tabs.forEach(tab => {
-      tab.addEventListener('click', () => {
-          tabs.forEach(t => t.classList.remove('active'));
-          tab.classList.add('active');
-          showActiveGallery(tab);
-      });
+    tab.addEventListener('click', () => {
+      tabs.forEach(t => t.classList.remove('active'));
+      tab.classList.add('active');
+      showActiveGallery(tab);
+    });
 
-      // Inicializar con la primera pestaña activa
-      document.addEventListener('DOMContentLoaded', () => {
+    // Inicializar con la primera pestaña activa
+    document.addEventListener('DOMContentLoaded', () => {
       const initialTab = document.querySelector('#pestañaShows');
       initialTab.classList.add('active');
       showActiveGallery(initialTab);
-      });
+    });
   });
 
   // Lógica para expandir la imagen al hacer clic y navegación por desplazamiento
@@ -235,36 +235,36 @@ document.addEventListener('DOMContentLoaded', () => {
   let currentImages = [];
 
   galeriaContent.addEventListener('click', (event) => {
-      if (event.target.tagName === 'IMG') {
-          const imgElements = event.target.closest('.carrusel').querySelectorAll('img');
-          currentImages = Array.from(imgElements);
-          currentImageIndex = currentImages.indexOf(event.target);
+    if (event.target.tagName === 'IMG') {
+      const imgElements = event.target.closest('.carrusel').querySelectorAll('img');
+      currentImages = Array.from(imgElements);
+      currentImageIndex = currentImages.indexOf(event.target);
 
-          showExpandedImage(currentImages[currentImageIndex].src);
-          expandedImgContainer.classList.add('show-expanded-img');
-          galeriaContent.classList.add('blur-background');
-      }
+      showExpandedImage(currentImages[currentImageIndex].src);
+      expandedImgContainer.classList.add('show-expanded-img');
+      galeriaContent.classList.add('blur-background');
+    }
   });
 
   expandedImgContainer.addEventListener('click', (event) => {
-      // Solo cerrar si se hace clic fuera de la imagen
-      if (event.target.tagName !== 'IMG') {
-          closeExpandedImage();
-      }
+    // Solo cerrar si se hace clic fuera de la imagen
+    if (event.target.tagName !== 'IMG') {
+      closeExpandedImage();
+    }
   });
 
   // Función para mostrar la imagen expandida
   function showExpandedImage(src) {
-      expandedImgContainer.innerHTML = '';
-      const expandedImg = document.createElement('img');
-      expandedImg.src = src;
-      expandedImgContainer.appendChild(expandedImg);
+    expandedImgContainer.innerHTML = '';
+    const expandedImg = document.createElement('img');
+    expandedImg.src = src;
+    expandedImgContainer.appendChild(expandedImg);
   }
 
   // Función para cerrar la imagen expandida
   function closeExpandedImage() {
-      expandedImgContainer.classList.remove('show-expanded-img');
-      galeriaContent.classList.remove('blur-background');
+    expandedImgContainer.classList.remove('show-expanded-img');
+    galeriaContent.classList.remove('blur-background');
   }
 
   // Manejo de gestos táctiles
@@ -272,12 +272,12 @@ document.addEventListener('DOMContentLoaded', () => {
   let touchEndX = 0;
 
   expandedImgContainer.addEventListener('touchstart', (event) => {
-      touchStartX = event.changedTouches[0].screenX;
+    touchStartX = event.changedTouches[0].screenX;
   });
 
   expandedImgContainer.addEventListener('touchend', (event) => {
-      touchEndX = event.changedTouches[0].screenX;
-      handleSwipeGesture();
+    touchEndX = event.changedTouches[0].screenX;
+    handleSwipeGesture();
   });
 
   // Manejo de eventos de arrastre del ratón
@@ -285,59 +285,73 @@ document.addEventListener('DOMContentLoaded', () => {
   let isDragging = false;
 
   expandedImgContainer.addEventListener('mousedown', (event) => {
-      mouseStartX = event.clientX;
-      isDragging = true;
-      expandedImgContainer.classList.add('dragging');
+    mouseStartX = event.clientX;
+    isDragging = true;
+    expandedImgContainer.classList.add('dragging');
   });
 
   expandedImgContainer.addEventListener('mousemove', (event) => {
-      if (isDragging) {
-          event.preventDefault(); // Previene la selección de texto mientras se arrastra
-      }
+    if (isDragging) {
+      event.preventDefault(); // Previene la selección de texto mientras se arrastra
+    }
   });
 
   expandedImgContainer.addEventListener('mouseup', (event) => {
-      if (isDragging) {
-          const mouseEndX = event.clientX;
-          handleMouseSwipe(mouseStartX, mouseEndX);
-          isDragging = false;
-          expandedImgContainer.classList.remove('dragging');
-      }
+    if (isDragging) {
+      const mouseEndX = event.clientX;
+      handleMouseSwipe(mouseStartX, mouseEndX);
+      isDragging = false;
+      expandedImgContainer.classList.remove('dragging');
+    }
   });
 
   expandedImgContainer.addEventListener('mouseleave', (event) => {
-      if (isDragging) {
-          const mouseEndX = event.clientX;
-          handleMouseSwipe(mouseStartX, mouseEndX);
-          isDragging = false;
-          expandedImgContainer.classList.remove('dragging');
-      }
+    if (isDragging) {
+      const mouseEndX = event.clientX;
+      handleMouseSwipe(mouseStartX, mouseEndX);
+      isDragging = false;
+      expandedImgContainer.classList.remove('dragging');
+    }
   });
 
   function handleSwipeGesture() {
-      if (touchEndX < touchStartX) {
-          // Desplazamiento hacia la izquierda
-          currentImageIndex = (currentImageIndex + 1) % currentImages.length;
-      } else if (touchEndX > touchStartX) {
-          // Desplazamiento hacia la derecha
-          currentImageIndex = (currentImageIndex - 1 + currentImages.length) % currentImages.length;
-      }
-      showExpandedImage(currentImages[currentImageIndex].src);
+    if (touchEndX < touchStartX) {
+      // Desplazamiento hacia la izquierda
+      currentImageIndex = (currentImageIndex + 1) % currentImages.length;
+    } else if (touchEndX > touchStartX) {
+      // Desplazamiento hacia la derecha
+      currentImageIndex = (currentImageIndex - 1 + currentImages.length) % currentImages.length;
+    }
+    showExpandedImage(currentImages[currentImageIndex].src);
   }
 
   function handleMouseSwipe(startX, endX) {
-      if (endX < startX) {
-          // Desplazamiento hacia la izquierda
-          currentImageIndex = (currentImageIndex + 1) % currentImages.length;
-      } else if (endX > startX) {
-          // Desplazamiento hacia la derecha
-          currentImageIndex = (currentImageIndex - 1 + currentImages.length) % currentImages.length;
-      }
-      showExpandedImage(currentImages[currentImageIndex].src);
+    if (endX < startX) {
+      // Desplazamiento hacia la izquierda
+      currentImageIndex = (currentImageIndex + 1) % currentImages.length;
+    } else if (endX > startX) {
+      // Desplazamiento hacia la derecha
+      currentImageIndex = (currentImageIndex - 1 + currentImages.length) % currentImages.length;
+    }
+    showExpandedImage(currentImages[currentImageIndex].src);
   }
 });
 
 
+// Barra de navegación en dispositivo móvil
+const navbarBoton = document.querySelector('#navbarBoton');
+const navbarImg = document.querySelector('#navbarImg');
+const navbar = document.querySelector('#navbarResponsive');
+
+navbarBoton.onclick = function () {
+  navbar.classList.toggle('open');
+
+  if (navbarImg.src.match('img/menu.svg')) {
+    navbarImg.src = 'img/close.svg'
+  } else {
+    navbarImg.src = 'img/menu.svg'
+  }
+}
 
 
 
