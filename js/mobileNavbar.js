2cba7d3a75c50359be4a7ad034bb1a54c4955aa8
Federico Manuel Peche <fedepeche@hotmail.com>
2024-06-02 19:56:35 -0300

    act
    cierre de menu navbar cuando se hace click por fuera del mismo

--- js/mobileNavbar.js
+++ js/mobileNavbar.js
@@ -2,25 +2,36 @@
 
 // Barra de navegación en dispositivo móvil
 document.addEventListener('DOMContentLoaded', () => {
-  const navbarBoton = document.getElementById('navbarBoton');
-  const navbarResponsive = document.getElementById('navbarResponsive');
-  const menuLinks = document.querySelectorAll('.navlist-responsive a');
-  const mainContent = document.querySelector('main');
-  const footerContent = document.querySelector('footer');
-
-  // Función para mostrar/ocultar el menú y aplicar/quitar el efecto blur
-  navbarBoton.addEventListener('click', () => {
-      navbarResponsive.classList.toggle('open');
-      mainContent.classList.toggle('blur'); // Agregar o quitar la clase blur al main
-      footerContent.classList.toggle('blur'); // Agregar o quitar la clase blur al footer
+    const navbarBoton = document.getElementById('navbarBoton');
+    const navbarResponsive = document.getElementById('navbarResponsive');
+    const menuLinks = document.querySelectorAll('.navlist-responsive a');
+    const mainContent = document.querySelector('main');
+    const footerContent = document.querySelector('footer');
+  
+    // Función para mostrar/ocultar el menú y aplicar/quitar el efecto blur
+    navbarBoton.addEventListener('click', () => {
+        navbarResponsive.classList.toggle('open');
+        mainContent.classList.toggle('blur'); // Agregar o quitar la clase blur al main
+        footerContent.classList.toggle('blur'); // Agregar o quitar la clase blur al footer
+    });
+  
+    // Función para cerrar el menú cuando se hace clic en un enlace y quitar el efecto blur
+    menuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navbarResponsive.classList.remove('open');
+            mainContent.classList.remove('blur'); // Quitar la clase blur al main
+            footerContent.classList.remove('blur'); // Quitar la clase blur al footer
+        });
+    });
+  
+    // Cerrar el menú cuando se hace clic fuera de él
+    document.addEventListener('click', (event) => {
+        const target = event.target;
+        if (!navbarResponsive.contains(target) && !navbarBoton.contains(target)) {
+            navbarResponsive.classList.remove('open');
+            mainContent.classList.remove('blur'); // Quitar la clase blur al main
+            footerContent.classList.remove('blur'); // Quitar la clase blur al footer
+        }
+    });
   });
-
-  // Función para cerrar el menú cuando se hace clic en un enlace y quitar el efecto blur
-  menuLinks.forEach(link => {
-      link.addEventListener('click', () => {
-          navbarResponsive.classList.remove('open');
-          mainContent.classList.remove('blur'); // Quitar la clase blur al main
-          footerContent.classList.remove('blur'); // Quitar la clase blur al footer
-      });
-  });
-});
+  
