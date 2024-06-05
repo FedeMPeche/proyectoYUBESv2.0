387fe58f0fce7872b0385a7cde17c319e460b5e3
Federico Manuel Peche <fedepeche@hotmail.com>
2024-06-05 19:19:36 -0300

    act
    act

--- js/footer.js
+++ js/footer.js
@@ -4,3 +4,17 @@
 function onSubmit(token) {
   document.getElementById("formularioContacto").submit();
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const formularioContacto = document.getElementById('formularioContacto');
+
+  formularioContacto.addEventListener('submit', (event) => {
+      event.preventDefault(); // Prevenir el envío del formulario temporalmente para mostrar la alerta
+
+      // Mostrar la alerta
+      alert("Muchas gracias por ponerte en contacto con YUBES, pronto nos estaremos comunicando!");
+
+      // Continuar con el envío del formulario después de mostrar la alerta
+      formularioContacto.submit();
+  });
+});
