246c4b75c89147a28c8588099ec660bf2335db47
Federico Manuel Peche <fedepeche@hotmail.com>
2024-06-07 15:32:47 -0300

    act
    imagen de background hasta la carga del video

---
+++ js/videoInicio.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var video = document.getElementById('background-video');
+
+    // Cuando el video se haya cargado completamente
+    video.addEventListener('canplaythrough', function() {
+        video.classList.add('loaded'); // Añadir la clase loaded al video
+    });
+
+    // Comienza a reproducir el video automáticamente
+    video.play();
+});
+
